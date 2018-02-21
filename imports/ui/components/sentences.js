91e4f087c2cf59b61eb09e175b926192e260db71
Ramazan ALTINSOY <ramazanaltinsoy@outlook.com>
2018-02-22 02:01:07 +0300

    değiştirildi: imports/ui/components/sentences.html 	değiştirildi: imports/ui/components/sentences.js 	silindi:      imports/ui/layouts/app-body0.html 	silindi:      imports/ui/layouts/app-body0.js 	değiştirildi: package-lock.json 	değiştirildi: package.json

--- imports/ui/components/sentences.js
+++ imports/ui/components/sentences.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Sentences } from '../../api/sentences/sentences.js';
+import { Words } from '../../api/words/words.js';
 import { insert, remove, update } from '../../api/sentences/methods.js';
 
 import './sentences.html';
@@ -10,12 +11,13 @@ import './sentences.html';
 Template.body.onCreated(function appBodyOnCreated() {
   this.state = new ReactiveDict();
   Meteor.subscribe("sentences.list");
+  Meteor.subscribe("words.list");
 
 });
 
 Template.word_details.helpers({
   dataDetails(){
-    isDetailOpen = Session.get('isDetailsOpen');
+    const isDetailOpen = Session.get('isDetailsOpen');
     const id = Session.get('wordId');
     if(isDetailOpen && id){
       let count=0;
@@ -25,6 +27,14 @@ Template.word_details.helpers({
       });
       return data;
     }
+    return null;
+  },
+  wordGet(){
+    const id = Session.get('wordId');
+    if(id){
+      return Words.findOne(id);
+    }
+    return null;
   },
 });
 
@@ -42,8 +52,27 @@ Template.word_details.events({
     });
     target.text.value = '';
   },
+  'submit .edit-sentence'(event){  ////////////////////////////////kalınan yer
+    event.preventDefault();
+    update.call({
+      _id:this._id,
+      wordId:this.wordId,
+      text:event.target.text.value // new sentence
+    });
+    var parent = document.getElementsByClassName("sentenceConvertToInput")[0];
+    var child = document.getElementsByClassName("edit-sentence")[0];
+    child.remove();
+    parent.innerHTML = event.target.text.value;
+  },
   'click #sentenceEdit'(event){
-    
+    event.preventDefault();
+
+    const tdDom = document.getElementsByClassName("sentenceConvertToInput")[this.rowCount-1];
+    tdDom.innerHTML='<form class="edit-sentence form-search">'+
+                      '<div class="input-append">'+
+                        '<input name="text" class="input-medium search-query" type="text" value="'+this.text+'">'+
+                        '<button type="submit" class="btn">Add</button>'+
+                    '</div></form>';
   },
   'click #sentenceDelete'(event){
     if(confirm('Are you sure? You are deleting: '+this.text)){
